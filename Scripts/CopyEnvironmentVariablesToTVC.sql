9fbb5d3e10c7ddc27f9b5c6df83e635fae502a77
agent86ray <45230312+agent86ray@users.noreply.github.com>
2019-01-27 16:46:12 -0500

    Added CopyEnvironmentVariablesToTVC script.

---
+++ Scripts/CopyEnvironmentVariablesToTVC.sql
@@ -0,0 +1,21 @@
+﻿-- SCRIPT NAME: CopyEnvironmentVariablesToTVC.sql
+-- Get the environment variable names and values from the folder name
+-- and environment name.  Format the results so they can be used in
+-- a table valued constructor (TVC).
+DECLARE @FOLDER_NAME NVARCHAR(128) = N'CRM';
+DECLARE @SOURCE_ENVIRONMENT NVARCHAR(128) = N'DEV';
+
+SELECT ',(' +
+    '''' + v.[name] + '''' + ',' +
+    '''' + CONVERT(NVARCHAR(1024),ISNULL(v.[value], N'<VALUE GOES HERE>')) + 
+	''''  + ',' +
+    '''' + v.[description] + '''' +
+    ')' ENVIRONMENT_VARIABLES
+FROM [SSISDB].[catalog].[environments] e
+JOIN [SSISDB].[catalog].[folders] f
+	ON e.[folder_id] = f.[folder_id]
+JOIN [SSISDB].[catalog].[environment_variables] v
+	ON e.[environment_id] = v.[environment_id]
+WHERE e.[name] = @SOURCE_ENVIRONMENT
+AND f.[name] = @FOLDER_NAME
+ORDER BY v.[name];
